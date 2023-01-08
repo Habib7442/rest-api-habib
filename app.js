389924f40db973d30011e922aa8a5cdd4c8b74bb
Habib7442 <habibtanwir1906@gmail.com>
2023-01-08 18:24:05 +0530

    rest-api

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const connectDB = require("./db/connect");
 
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
-const products_routes = require("./routes/products")
+const products_routes = require("./routes/products");
 
-app.get("/", (req,res) => {
-res.send("Hi, I am Habib")
-})
+app.get("/", (req, res) => {
+  res.send("Hi, I am Habib");
+});
 
 app.use("/api/products", products_routes);
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGODB_URL);
-        app.listen(PORT, () => {
-            console.log(`${PORT} Server is running`);
-        })
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => {
+      console.log(`${PORT} Server is running`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
-
